896ba11251020e9e49d0d096ff0c5daf4ac23aa6
Claire <claire.deshayes71@gmail.com>
2017-12-05 19:12:27 +0100

    correction 2 : free?

--- liste_generique.adb
+++ liste_generique.adb
@@ -1,5 +1,7 @@
 with Ada.Text_IO;
 with Ada.Integer_Text_IO;
+with Ada.Unchecked_Deallocation ;
+
 use Ada.Text_IO;
 use Ada.Integer_Text_IO;
 
@@ -13,6 +15,10 @@ package body Liste_Generique is
    type Iterateur_Interne is record
       L_it       : Liste;
    end record;
+
+   procedure free_Liste is new Ada.Unchecked_Deallocation(Cellule,Liste) ;
+   procedure free_Iterateur is new Ada.Unchecked_Deallocation(Iterateur_Interne,Iterateur);
+
    -------------------
    -- Affiche_Liste --
    -------------------
@@ -47,12 +53,18 @@ package body Liste_Generique is
    ------------------
 
    procedure Libere_Liste (L : in out Liste) is
-      It : Iterateur;
+      L_suiv, L_cour : Liste;
    begin
-      It := Creer_Iterateur(L);
-      while A_Suivant(It) loop
-       Suivant(It);
+      L_cour := L;
+      L_suiv := L.suivant;
+      while L_suiv /= null loop
+         free_Liste(L_cour);
+         L_cour := L_suiv;
+         L_suiv := L_suiv.suivant;
       end loop;
+      free_Liste(L_cour);
+      free_Liste(L_suiv);
+
 
    end Libere_Liste;
 
@@ -83,9 +95,7 @@ package body Liste_Generique is
 
    procedure Libere_Iterateur (It : in out Iterateur) is
    begin
-      --  Generated stub: replace with real body!
-      pragma Compile_Time_Warning (Standard.True, "Libere_Iterateur unimplemented");
-      raise Program_Error with "Unimplemented procedure Libere_Iterateur";
+      free_Iterateur(It);
    end Libere_Iterateur;
 
    -------------
