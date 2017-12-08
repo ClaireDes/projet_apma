f9f6ad6bff8e563b5b65467b71720836bf6b7fbc
francois_unix <francois.combe@phelma.grenoble-inp.fr>
2017-12-08 14:03:01 +0100

    eferfe

--- moteur_jeu.adb
+++ moteur_jeu.adb
@@ -7,13 +7,13 @@ package body moteur_jeu  is
    begin
       Coups := Coups_Possibles(E,JoueurMoteur);
       --for(i=0;i<Coups'Length;i++) loop
-         
-         
+
+
       --end loop;
-      
+
 
    end Choix_Coup;
-   
+
    --parametres : etat, profondeur max de recherche
    --retourn gain attendu pour cet etat
    function Eval_Min_Max(E : Etat; P : Natural; C : Coup; J : Joueur) return Integer is
@@ -21,15 +21,15 @@ package body moteur_jeu  is
       --on a atteint un feuille : evaluation statique
       if (P = 0) then
          return Eval(E);
-         
-      --on a atteint un etat terminal   
+
+      --on a atteint un etat terminal
       elsif Est_Gagnant(E,J) then
          return largeur*hauteur;
       elsif EstGagnant(E,Adversaire(J)) then
          return -largeur*hauteur;
       elsif Est_Nul(E) then
          return 0;
-         
+
       else
          --si c'est a l'adversaire de jouer
          if ((P/2)=1) then
@@ -38,9 +38,9 @@ package body moteur_jeu  is
             return Max(Coups_Possibles(E,J));
          end if;
       end if;
-      
+
    end Eval_Min_Max;
-   
-   
-   
+
+
+
 end moteur_jeu;
