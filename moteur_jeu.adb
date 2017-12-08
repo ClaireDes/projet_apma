10fd4daf042eeb51cc2ec33bcd1f49941c917316
Claire <claire.deshayes71@gmail.com>
2017-12-08 18:47:43 +0100

    ouhla on va tout changer

--- moteur_jeu.adb
+++ moteur_jeu.adb
@@ -4,25 +4,36 @@ package body moteur_jeu  is
 
    function Choix_Coup(E                    : Etat) return Coup  is
       Coups                                 : Liste_Coups.Liste;
-      Gain_max,Gain_Courant, Indice_Max :=0 : Integer;
+      Gain_max,Gain_Courant : Integer;
       Meilleur_Coup                         : Coup;
+      It : Iterateur;
    begin
       Coups := Coups_Possibles(E,JoueurMoteur);
-      for(i=Coups'First;i<Coups'Length;i++) loop
-         Gain_Courant := Eval_Min_Max(E,2,Coups[i],JoueurMoteur);
+      It := Creer_Iterateur(Coups);
+      Gain_Courant := Eval_Min_Max(E,2,Element_Courant(It),JoueurMoteur);
+      Gain_Courant := Gain_max;
+      while (Element_Courant /= null) loop
+         Gain_Courant := Eval_Min_Max(E,2,Element_Courant(It),JoueurMoteur);
          if Gain_Courant > Gain_max then
-            Indice_Max := i;
-         end if;
+            Meilleur_Coup := Element_Courant(It);
+             end if;
+         Suivant(It);
       end loop;
-      return Coups[Indice_Max];
+      return Meilleur_Coup;
    end Choix_Coup;
 
    --parametres                             : etat, profondeur max de recherche
    --retourn gain attendu pour cet etat
    function Eval_Min_Max(E : Etat; P : Natural; C : Coup; J : Joueur) return Integer is
-      P_courant := P                        : Natural
-      begin
-
+      P_Courant                      : Natural;
+        Coups : Liste_Coups;
+      Gain_Courant, Gain_Min , Gain_Max : Integer;
+      Coup_Choisi : Coup;
+      It : Iterateur;
+   begin
+      Gain_Max := -largeur*hauteur;
+      Gain_Min := - Gain_Max;
+         P_Courant := P;
          while P_Courant > 0 loop
 
 
@@ -38,22 +49,42 @@ package body moteur_jeu  is
                --si c'est a l'adversaire de jouer on prend le min
                if ((P/2)=1) then
 
-                  return Eval_Min_Max(E,P_Courant-1, Min(Coups_Possibles(E,Adversaire(J)),J);
-                                      else
+               Coups := Coups_Possibles(E,Adversaire(J));
+               It := Creer_Iterateur(Coups);
+               while (Element_Courant(It) /= null) loop
+                  Gain_Courant := Eval_Min_Max(E, P_Courant-1, Element_Courant(It), J);
+                  if Gain_Courant < Gain_Min then
+                     Gain_Min := Gain_Courant;
+                     Coup_Choisi := Element_Courant(It);
+                  end if;
+                    Suivant(It);
+               end loop;
+
+               else
+
+               Coups := Coups_Possibles(E,Adversaire(J));
+               It := Creer_Iterateur(Coups);
+               while (Element_Courant(It) /= null) loop
+                  Gain_Courant := Eval_Min_Max(E, P_Courant-1, Element_Courant(It), J);
+                  if Gain_Courant >= Gain_Max then
+                     Gain_Max := Gain_Courant;
+                     Coup_Choisi := Element_Courant(It);
+                  end if;
+                    Suivant(It);
+               end loop;
+
+                  return Coup_Choisi;
+
+               end if;
+            end if;
+
+         P_Courant := P_Courant -1;
+      end loop;
+      return Eval(E);
 
 
-                                         return Eval_Min_Max(E,P_Courant-1,Max(Coups_Possibles(E,J)),J);
-                                      end if;
-                                      end if;
-
-                                      P_Courant := P_Courant -1;
-                                      end loop;
-                                      return Eval(E);
-
-
-                                      end Eval_Min_Max;
+   end Eval_Min_Max;
 
 
 
-                                      end moteur_jeu;
-
+end moteur_jeu;
--- puissance4.adb
+++ puissance4.adb
@@ -188,4 +188,29 @@ begin
   return true;
 end Est_Plein;
 
+   -- Retourne la liste des coups possibles pour J a partir de l'etat
+   function Coups_Possibles(E : Etat; J : Joueur) return Liste_Coups.Liste is
+      C : Integer;
+      Cp : Coup;
+      L : Liste_Coups.Liste;
+   begin
+      for i in 0..(hauteur-1) loop
+         for C in 0..(largeur-1) loop
+            if E(C + largeur*i) = Vide then
+               Cp.Col := C;
+               Cp.J:=J;
+               Insere_Tete(Cp,L);
+            end if;
+            C := C + 1;
+         end loop;
+      end loop;
+      return L;
+   end Coups_Possibles;
+
+   -- Evaluation statique du jeu du point de vue de l'ordinateur
+   function Eval(E : Etat) return Integer is
+   begin
+
+   end Eval;
+
 end Puissance4;
