887d3af6ed585881021583e2f288be64e975be62
Claire <claire.deshayes71@gmail.com>
2017-12-08 16:17:07 +0100

    EvalMinMAx, reste a implementer une fonction de recherche du minimum sur les coups - peut-être faire une liste de (coups, gain) au lieu d'une liste de coups simple

--- moteur_jeu.adb
+++ moteur_jeu.adb
@@ -2,13 +2,12 @@
 
 package body moteur_jeu  is
 
-   function Choix_Coup(E : Etat) return Coup  is
-      Coups : Liste_Coups.Liste;
+   function Choix_Coup(E                    : Etat) return Coup  is
+      Coups                                 : Liste_Coups.Liste;
       Gain_max,Gain_Courant, Indice_Max :=0 : Integer;
-      Meilleur_Coup : Coup;
+      Meilleur_Coup                         : Coup;
    begin
       Coups := Coups_Possibles(E,JoueurMoteur);
-<<<<<<< HEAD
       for(i=Coups'First;i<Coups'Length;i++) loop
          Gain_Courant := Eval_Min_Max(E,2,Coups[i],JoueurMoteur);
          if Gain_Courant > Gain_max then
@@ -16,43 +15,45 @@ package body moteur_jeu  is
          end if;
       end loop;
       return Coups[Indice_Max];
-=======
-      --for(i=0;i<Coups'Length;i++) loop
-
-
-      --end loop;
-
-
->>>>>>> f9f6ad6bff8e563b5b65467b71720836bf6b7fbc
    end Choix_Coup;
 
-   --parametres : etat, profondeur max de recherche
+   --parametres                             : etat, profondeur max de recherche
    --retourn gain attendu pour cet etat
    function Eval_Min_Max(E : Etat; P : Natural; C : Coup; J : Joueur) return Integer is
-   begin
-      --on a atteint un feuille : evaluation statique
-      if (P = 0) then
-         return Eval(E);
+      P_courant := P                        : Natural
+      begin
 
-      --on a atteint un etat terminal
-      elsif Est_Gagnant(E,J) then
-         return largeur*hauteur;
-      elsif EstGagnant(E,Adversaire(J)) then
-         return -largeur*hauteur;
-      elsif Est_Nul(E) then
-         return 0;
-
-      else
-         --si c'est a l'adversaire de jouer
-         if ((P/2)=1) then
-            return Min(Coups_Possibles(E,Adversaire(J)));
-         else
-            return Max(Coups_Possibles(E,J));
-         end if;
-      end if;
-
-   end Eval_Min_Max;
+         while P_Courant > 0 loop
 
 
+            --on a atteint un etat terminal
+            if Est_Gagnant(E,J) then
+               return largeur*hauteur;
+            elsif Est_Gagnant(E,Adversaire(J)) then
+               return -largeur*hauteur;
+            elsif Est_Nul(E) then
+               return 0;
 
-end moteur_jeu;
+            else
+               --si c'est a l'adversaire de jouer on prend le min
+               if ((P/2)=1) then
+
+                  return Eval_Min_Max(E,P_Courant-1, Min(Coups_Possibles(E,Adversaire(J)),J);
+                                      else
+
+
+                                         return Eval_Min_Max(E,P_Courant-1,Max(Coups_Possibles(E,J)),J);
+                                      end if;
+                                      end if;
+
+                                      P_Courant := P_Courant -1;
+                                      end loop;
+                                      return Eval(E);
+
+
+                                      end Eval_Min_Max;
+
+
+
+                                      end moteur_jeu;
+
